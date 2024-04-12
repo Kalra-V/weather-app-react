13df2815932bfd5b62581672df936420fdcaa5b9
Vansh Kalra <vanshkalra645@gmail.com>
2024-04-12 23:53:38 +0530

    Completed search component

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f3db0524e5mshce5cf6a1f50826ap13bf12jsn52ed1983717c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
