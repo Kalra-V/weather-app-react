0effb721dff54048b907a6857108ead46504e92f
Vansh Kalra <vanshkalra645@gmail.com>
2024-04-23 22:01:13 +0530

    Fetching and mapping data from weather API

--- src/components/api.js
+++ src/components/api.js
@@ -7,3 +7,7 @@ export const geoApiOptions = {
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "4fcf82b95d3190dda5ab19a09ab679fe";
