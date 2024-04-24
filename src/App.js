5aed62f7c1a45d13f549cde8b5577cf348060f15
Vansh Kalra <vanshkalra645@gmail.com>
2024-04-25 00:47:30 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_URL } from "./components/api";
 import { WEATHER_API_KEY } from "./components/api";
 import { useState } from "react";
+import Forecast from "./components/forecast/forecast";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -38,6 +39,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
